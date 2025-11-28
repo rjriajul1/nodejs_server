03860b918437c58e0c40d89fe0054fbaa4dbb7dc
Riajul Islam <rjriajulislam9@gmail.com>
2025-11-29 03:38:14 +0600

    custom route handler

---
+++ src/helper/RouteHandle.ts
@@ -0,0 +1,13 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+type RouterHandler = (req: IncomingMessage, res: ServerResponse) => void;
+const routes: Map<string, Map<string, RouterHandler>> = new Map();
+
+function addRoutes(method: string, path: string, handler: RouterHandler) {
+  if (!routes.has(method)) {
+    routes.set(method, new Map());
+  }
+  routes.get(method)!.set(path, handler);
+}
+
+export default addRoutes;
